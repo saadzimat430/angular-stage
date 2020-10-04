e8d987d551871beb7d3a728c6f5ebbcedb6e52cc
PC <saadzimat430@gmail.com>
2020-10-04 16:55:05 +0100

    commit

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/static'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/static/index.html'));
+    res.sendFile(path.join(__dirname+'/static/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
