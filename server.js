6f0b9e8aca4dd7bd8d5e31b3c25e9c7d37d38647
PC <saadzimat430@gmail.com>
2020-10-09 11:18:22 +0100

    commit

--- server.js
+++ server.js
@@ -13,5 +13,3 @@ app.get('/*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
